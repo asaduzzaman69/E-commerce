4614480c3f5b04257a4564f73c002e93f9c5e637
asaduzzaman69 <asaduzzamanhimel12@gmail.com>
2020-06-09 14:29:13 +0800

    adding new file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import HomePage from './homepage.component'
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <HomePage />
 
     </div>
   );
